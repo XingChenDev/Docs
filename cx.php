01a3cfac58e9defff15d23edeec9d9d8335df2e7
stardevel <stardevel@outlook.com>
2024-02-19 21:22:13 +0800

    更改cx.php代码

--- cx.php
+++ cx.php
@@ -5,9 +5,9 @@
       return json_encode ($json_string,JSON_UNESCAPED_UNICODE);
    };
    if($name==""){
-      echo open_file("./plugins/404.json"); 
-   }else if(file_exists("./plugins/$name.json")){ 
-      echo open_file("./plugins/$name.json"); 
+      echo open_file("./update/404.json"); 
+   }else if(file_exists("./update/$name.json")){ 
+      echo open_file("./update/$name.json"); 
    }else{
       $test = array("name"=>"错误","status"=>4000,"message"=>"查询不到该文件","version"=>"","fixvers"=>"","updatemes"=>"{}" );
       echo  json_encode($test,JSON_UNESCAPED_UNICODE);
